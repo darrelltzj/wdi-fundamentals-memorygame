ade8f4d0c8854729848b84e5a3c7a8f53a69ca6c
Darrell Teo <darrelltzj@Darrells-MacBook-Air.local>
2017-01-24 22:21:48 +0800

    v03_20170124

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,28 @@
-console.log("JS file is connected to HTML! Woo!")
+// console.log("JS file is connected to HTML! Woo!")
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	alert("You found a Match!");
+}
+// else if (cardOne === cardTwo) {
+
+// }
+// else if (cardOne === cardThree) {
+
+// }
+// else if (cardOne === cardFour) {
+
+// }
+// else if (cardTwo === cardThree) {
+
+// }
+// else if (cardThree === cardFour) {
+
+// }
+else{
+	alert("Sorry, try again.");
+}
